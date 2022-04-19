7dbbf3a1331f25174294eff1b1fc2de00cbd6727
Xelphos <xelphos@gmail.com>
2022-04-19 19:00:38 -0400

    Allow for fetching data of all release channels

--- discordversion.js
+++ discordversion.js
@@ -1,33 +1,49 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-const getIndexOfFiles = async () => {
-    return fetch('https://canary.discordapp.com/app')
+const releaseChannel = ['https://discordapp.com/', 'https://ptb.discordapp.com/', 'https://canary.discordapp.com/']; 
+const releaseChannelName = ['Stable: ', 'Beta: ', 'Canary: ']
+
+const getIndexOfFiles = async (channel) => {
+    const result = await fetch(`${channel}app`)
         .then((response) => {
             return response.text();
         })
         .then((data) => {
             return data.match(/assets\/[\w\d]+\.js/g).reverse();
         })
+    return result;
 }
 
-const scrapeIndexFiles = async (files) => {
-    return fetch(`https://canary.discord.com/${files}`)
+const scrapeIndexFiles = async (channel, fileIndex) => {
+    const result = await fetch(`${channel}${fileIndex}`)
         .then((response) => {
             return response.text();
         })
         .then((data) => {
             return data.match(/Build Number: [\w.]+, Version Hash: [\w.]+/g);
         })
+    return result;
 }
 
-const getDiscordBuild = async () => {
-    const indexOfFiles = await getIndexOfFiles();    
-    for (const file in indexOfFiles) {        
-        const scrapeIndex = await scrapeIndexFiles(indexOfFiles[file]);
+const getIndividualReleaseBuild = async (release) => {
+    const indexOfFiles = await getIndexOfFiles(releaseChannel[release]);
+    for (const file in indexOfFiles) {
+        const scrapeIndex = await scrapeIndexFiles(releaseChannel[release], indexOfFiles[file]);
         if (scrapeIndex) return scrapeIndex;
-    }   
+    }
 }
 
-console.log(await getDiscordBuild())
+const getAllReleaseBuilds = async () => {
+    let discordReleases = [];
+    for (i = 0; i < releaseChannel.length; i++) {
+        const result = await getIndividualReleaseBuild(i);
+        discordReleases.push(releaseChannelName[i] + result);
+    }
+    return discordReleases;
+}
+
+(async () => {
+    console.log(await getAllReleaseBuilds())
+})()
 
 
