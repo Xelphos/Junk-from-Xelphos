28e167278c57dd659e9b6b3d6dfe56dfd050cdfe
Xelphos <87526465+Xelphos@users.noreply.github.com>
2022-04-20 13:27:34 -0400

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,4 +0,0 @@
-let a = prompt(1, "First number?");
-let b = prompt(2, "Second number?");
-
-alert(a + b); // 12
--- scripts/test.js
+++
@@ -1,7 +0,0 @@
-const string = 'This is a "string!"';
-const doubleQuote = "Double quoted string!"
-const singleQuote = 'Single quoted string!'
-
-let badString =  string;
-
-console.log(badString, doubleQuote, singleQuote);
