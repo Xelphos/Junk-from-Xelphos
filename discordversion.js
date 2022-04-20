391a7a044577b46ccf0356c85cef3e07e07bdb9b
Xelphos <xelphos@gmail.com>
2022-04-20 13:11:07 -0400

    Make automatic on a timer of 15 seconds

--- discordversion.js
+++ discordversion.js
@@ -1,17 +1,29 @@
 const fetch = require('node-fetch');
-
+const fs = require('fs');
+const storedHashPath = './discordhash.json';
+const settingsPath = './settings.json';
 const releaseChannel = ['https://discordapp.com/', 'https://ptb.discordapp.com/', 'https://canary.discordapp.com/']; 
-const releaseChannelName = ['Stable: ', 'Beta: ', 'Canary: ']
+const releaseChannelName = ['Stable', 'Beta', 'Canary']
 
-const getIndexOfFiles = async (channel) => {
-    const result = await fetch(`${channel}app`)
+
+const getIndexOfFiles = async (channel, index) => {
+    const storedDiscordHash = JSON.parse(fs.readFileSync(storedHashPath));
+    const retrieveData = await fetch(`${channel}app`)
         .then((response) => {
             return response.text();
         })
-        .then((data) => {
-            return data.match(/assets\/[\w\d]+\.js/g).reverse();
-        })
-    return result;
+
+    const getBuildId = retrieveData.match(/"buildId":"([\w\d]+)"/).reverse();
+    const foundBuildId = getBuildId[0];
+
+    if (storedDiscordHash[releaseChannelName[index]] !== foundBuildId) {
+        storedDiscordHash[releaseChannelName[index]] = foundBuildId;
+        fs.writeFileSync(storedHashPath, JSON.stringify(storedDiscordHash, null, 4));
+    } else {
+        return false;
+    }
+
+    return retrieveData.match(/assets\/[\w\d]+\.js/g).reverse();
 }
 
 const searchIndexOfFiles = async (channel, fileIndex) => {
@@ -20,13 +32,18 @@ const searchIndexOfFiles = async (channel, fileIndex) => {
             return response.text();
         })
         .then((data) => {
-            return data.match(/Build Number: [\w.]+, Version Hash: [\w.]+/g);
+            let searchMatch = data.match(/Build Number: ([\w.]+), Version Hash: ([\w.]+)/);
+            if (searchMatch ===  null) return;
+            let matchResult = []
+            matchResult.push(searchMatch[1], searchMatch[2])
+            return matchResult;
         })
     return result;
 }
 
 const getIndividualReleaseBuild = async (release) => {
-    const indexOfFiles = await getIndexOfFiles(releaseChannel[release]);
+    const indexOfFiles = await getIndexOfFiles(releaseChannel[release], release);
+    if (indexOfFiles === false) return false;
     for (const file in indexOfFiles) {
         const indexSearchResults = await searchIndexOfFiles(releaseChannel[release], indexOfFiles[file]);
         if (indexSearchResults) return indexSearchResults;
@@ -35,15 +52,35 @@ const getIndividualReleaseBuild = async (release) => {
 
 const getAllReleaseBuilds = async () => {
     let discordReleases = [];
-    for (i = 0; i < releaseChannel.length; i++) {
+    for (let i = 0; i < releaseChannel.length; i++) {
         const result = await getIndividualReleaseBuild(i);
-        discordReleases.push(releaseChannelName[i] + result);
+        discordReleases.push(result);
     }
     return discordReleases;
 }
 
+const sendDiscordUpdate = async () => {
+    let getSetting = JSON.parse(fs.readFileSync(settingsPath));
+    while (getSetting.botSettings.discordUpdates === true) {
+        let discordUpdateData =  await getAllReleaseBuilds();
+        for (let i = 0; i < discordUpdateData.length; i++) {
+            if (discordUpdateData[i]) {
+                console.log(discordUpdateData[i])
+                let buildNumber = discordUpdateData[i][0]
+                let versionHash = discordUpdateData[i][1]
+                console.log(buildNumber, versionHash)
+            }
+        }
+        getSetting = JSON.parse(fs.readFileSync(settingsPath));
+        await delay(15000);
+    }
+    return;
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 (async () => {
-    console.log(await getAllReleaseBuilds())
+    console.log(await sendDiscordUpdate())
 })()
 
 
